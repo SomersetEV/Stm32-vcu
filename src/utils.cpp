ba66f2dc370d7f54930d9af8d393575d7516b2ff
Ben Bament <bbament@gmail.com>
2025-10-21 14:41:37 +0100

    code tidy 2

--- src/utils.cpp
+++ src/utils.cpp
@@ -259,12 +259,12 @@ void SelectDirection(Vehicle *vehicle, Shifter *shifter) {
       else if (Param::GetBool(Param::din_reverse))
         userDirSelection = -1 * dirSign;
     }
-  if(Param::GetInt(Param::PlugDet) == 1) // If charge socket is not empty. Force Neutral
-  {
-    userDirSelection = 0;
-  }
-
-  }
+   if (Param::GetInt(Param::PlugDet) ==
++        1) // If charge socket is not empty. Force Neutral
++    {
++      userDirSelection = 0;
++    }
+   }
 
   // Change Allowed Logic//
 
